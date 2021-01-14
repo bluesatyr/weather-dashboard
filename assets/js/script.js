a6c56359b29bc09d7e7def51136f81bd6875dcbf
bluesatyr <shawnevans.music@gmail.com>
2021-01-14 11:30:38 -0500

    initial code structure

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var weatherObject = {
+    city: "Toronto",
+    date: "1/14/21",
+    tempC: "10",
+    windSpeed: "20 KPH",
+    uvIndex: "8.0",
+}
+
+var fiveDay = {
+    {date: "1/15/21",
+    }
+}
