0ac60c3ee0865098677dd80a72eaa26cb5ffe097
bluesatyr <shawnevans.music@gmail.com>
2021-01-16 15:58:20 -0500

    render previous/popular searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,27 @@
+// global variables
 var locApiUrl = "http://api.openweathermap.org/data/2.5/weather?q="
 var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?";
 var city = "";
-
 var apiKey = "587e6a88adff4eb1878a93d9f751bd59";
-var previousCities = ["toronto", "london", "sydney", "tokyo"];
-    
+var previousCities = [];
+  
 var getPreviousCities = function () {
     // city list ul element
     var cityList = document.querySelector('.city-list');
+    cityList.innerHTML = "";
     // get cities as array from localStorage
     var citiesArray = localStorage.getItem('cities');
-    previousCities = JSON.parse(citiesArray);
+    if (!citiesArray) {
+        previousCities = ["london", "sydney", "tokyo", "toronto"];
+    } else {
+        previousCities = JSON.parse(citiesArray);
+    };
     
     // creates a list of previous cities: most recent first
     for (var i = previousCities.length - 1; i > -1 ; i--) {
         console.log(previousCities[i])
         var city = document.createElement('li');
+        city.classList = 'previous capitalize'
         city.textContent = previousCities[i];
         cityList.appendChild(city);
     }; 
@@ -23,48 +29,25 @@ var getPreviousCities = function () {
 
 var saveCities = function(cities){
     if (cities.length > 10) {
-        citiesities.shift();
+        cities.shift();
     };
     localStorage.setItem('cities', JSON.stringify(cities));      
 }
-
-
-
-
-
-/* 
-
-var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-var iconcode = data.current.weather.icon;
-
-*/
-
-
 
 var weatherItems = ["Temperature",  "Humidity", "Wind Speed", "UV Index"];
 var objKeys = ["tempC", "humid", "windSpeed", "uvIndex"];
 
 
 
-var fiveDay = [
-    {date: "1/15/21", icon: "sun", temp: "12", humid: "44%"},
-    {date: "1/16/21", icon: "sun", temp: "13", humid: "45%"},
-    {date: "1/17/21", icon: "sun", temp: "14", humid: "46%"},
-    {date: "1/18/21", icon: "sun", temp: "15", humid: "47%"},
-    {date: "1/19/21", icon: "sun", temp: "16", humid: "48%"}
-    ];
-
-
 var createFiveDayArray = function (data) {
     var forecastArray = [];
-    for (var i = 0; i < 5; i++) {
+    for (var i = 1; i < 6; i++) {
         var maxTemp = (data.daily[i].temp.max - 273.15);
         var temp = maxTemp.toFixed(1);
         var dayTime = data.daily[i].dt * 1000;
         var timeZone = data.timezone_offset * 1000;
         var dayDate = moment(dayTime + timeZone);
         var formattedDate = dayDate.format('M/DD/YYYY');
-        console.log(data.daily[i].weather[0].icon);
         var day = {
             date: formattedDate,
             icon: data.daily[i].weather[0].icon,
@@ -74,37 +57,10 @@ var createFiveDayArray = function (data) {
         
         forecastArray.push(day);
     };
-    console.log(forecastArray);
     return forecastArray;
 }
 
-// creates the curent weather section from returned JSON object(s)
 
-// uv index info: https://19january2017snapshot.epa.gov/sunsafety/uv-index-scale-1_.html
-
-/* var createCurrentEl = function(Obj) {
-    var results = document.querySelector('.results');
-    
-    var currentWeatherEl = document.createElement('div');
-    currentWeatherEl.className = 'current-weather';
-    
-    var city = document.createElement('h2'); // use text-transform: capitalize; in css
-    city.className = 'capitalize'
-    city.textContent = Obj.city + " (" + Obj.date + ")";
-    console.log(city);
-    
-    currentWeatherEl.appendChild(city);
-    results.appendChild(currentWeatherEl);
-    console.log(results);
-    
-    for (var i = 0; i < 4; i++) {
-        let item = document.createElement('p');
-        console.log(item);
-        item.textContent = weatherItems[i] + ": " + Obj[objKeys[i]];
-        console.log(item);
-        currentWeatherEl.appendChild(item);
-    } 
-};  */
  
 var uvColor = function(uvi) {
     var uviEl = document.querySelector('#uvi');
@@ -157,8 +113,9 @@ var search = function (location) {
     
 };
 
-
+// mainapi calls
 var getWeather = function (city) {
+    // to get latitude and longitude for 2nd api call
     var firstUrl = locApiUrl + city + '&units=metric&appid=' + apiKey;
     console.log(firstUrl);
     fetch(firstUrl)
@@ -167,9 +124,8 @@ var getWeather = function (city) {
         })
         .then(function (res){
             var long = res.coord.lon;
-            console.log(long);
             var lat = res.coord.lat;
-            console.log(lat);
+            // 2nd api call to get all weather and forecast data
             var nextUrl = oneCallUrl + "lat=" + lat + "&lon=" + long + "&exclude=minutely,hourly,alerts&appid=" + apiKey;
             console.log(nextUrl);
             return fetch(nextUrl)
@@ -178,17 +134,16 @@ var getWeather = function (city) {
             return data.json();
         })
         .then(function(data) {
-            // createCurrentEl(data);
-            // createFiveDayEl(data);
+            // adjust returned data to be in the correct formats
             var currentTemp = (data.current.temp - 273.15);
             var temp = currentTemp.toFixed(1);
             var currentTime = data.current.dt * 1000;
             var timeZone = data.timezone_offset * 1000;
-            
             var currentDate = moment(currentTime + timeZone);
             var formattedDate = currentDate.format('M/DD/YYYY');
+            
             var wIcon = data.current.weather[0].icon;
-                   
+            // creates single object to be passed to createCurrentEl function       
             var weatherObj = {
                 city: city,
                 date: formattedDate,
@@ -198,27 +153,28 @@ var getWeather = function (city) {
                 windSpeed: data.current.wind_speed,
                 uvIndex: data.current.uvi
             };
-            
+            // creates an array to be passed to the createFiveDayEl function
             var forecastArray = createFiveDayArray(data);
-
+            // function calls to render current weather and forecast elements
             createCurrentEl(weatherObj);
             createFiveDayEl(forecastArray);
-        })
+            getPreviousCities();
+        }).catch(function(error) {
+            alert('Request failed:', error.message);
+        });
 }
 
-
-
-
-
+// submit user input to fetch and render City's weather and forecast
 var searchHandler = function(event) {
     event.preventDefault();
     var cityInput = document.querySelector('#search-input').value;
     city = cityInput.toLowerCase().trim();
-    console.log(city);
     previousCities.push(city);
     saveCities(previousCities);
     getWeather(city);
-    cityInput.textContent = "";
-}
+};
 
+getPreviousCities();
+
+// event handlers
 document.querySelector('#search-btn').addEventListener('click', searchHandler);
